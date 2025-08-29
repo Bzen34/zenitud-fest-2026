a4642110a281623b084c9fa789bb1d4168ef7b7a
Bzen34 <b.zen.association@gmail.com>
2025-08-29 15:29:21 +0200

    Delete sw-register.js

--- sw-register.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js')
-    .then(reg => console.log('Service Worker enregistré ✅'))
-    .catch(err => console.error('Erreur SW ❌', err));
-}
