7b3bd9896dfc4824566477b2ddddd17172a27725
Bzen34 <b.zen.association@gmail.com>
2025-08-29 16:33:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-// Future JavaScript functionality can be added here
+// Message de bienvenue dans la console
 console.log("Bienvenue au Festival Zenitude 2026");
+
+// Fonction pour gérer la soumission du formulaire de contact
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Empêche le rechargement de la page
+    alert("Votre message a été envoyé !"); // Alerte de confirmation
+});
