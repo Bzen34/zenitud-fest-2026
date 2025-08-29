989bc0f05d4357148f97b455eefe30b3c1266b4e
Bzen34 <b.zen.association@gmail.com>
2025-08-29 14:14:38 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installé');
+  event.waitUntil(
+    caches.open('zenitud-cache-v1').then((cache) => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icon-192.png',
+        './icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== 'zenitud-cache-v1' ? caches.delete(k) : null)))
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => resp || fetch(event.request))
+  );
+});
